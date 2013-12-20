d84a4fa43513594709ed57fdb060af86a356e583
Andy Buntine <bunts@hhd.com.au>
2013-12-20 17:27:20 +1100

    Added model generator

--- lib/generators/traka/model_generator.rb
+++ lib/generators/traka/model_generator.rb
@@ -1,11 +1,12 @@
 module Traka
   module Generators
     class ModelGenerator < ::Rails::Generators::Base
-      #source_root File.expand_path("../templates", __FILE__)
       desc "Generates the model for recording trakable changes"
 
       def create_model
-        create_file Rails.root.join("config", "ghey.rb"), "This is ghey"
+        generate "model", "klass:string uuid:string action_type:string version:integer"
+
+        # Whatever changes need to be made to the generated model go here...
       end
     end
   end
