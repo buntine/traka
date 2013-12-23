9adbcd22a59df8461cf6866ac13bce0ad7623596
Andy Buntine <bunts@hhd.com.au>
2013-12-23 15:58:31 +1100

    Started uninstall generator

--- lib/generators/traka/install_generator.rb
+++ lib/generators/traka/install_generator.rb
@@ -9,13 +9,8 @@ module Traka
         generate "model", "traka_change klass:string uuid:string action_type:string version:integer"
       end
 
-#      def update_model
-#        copy_file "traka_change.rb", "app/models/traka_change.rb"
-#      end
-
       def create_version_file
         directory "public/system"
-        #copy_file "version.txt", "public/system/api/version.txt"
       end
     end
   end
