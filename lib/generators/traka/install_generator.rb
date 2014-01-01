3c8f7c80891c968bedcb9b81f2edc5785d2f4f98
Andy Buntine <bunts@hhd.com.au>
2014-01-01 20:10:24 +1100

    Reshuffling generators

--- lib/generators/traka/install_generator.rb
+++ lib/generators/traka/install_generator.rb
@@ -1,12 +1,16 @@
+require 'rails/generators/migration'
+
 module Traka
   module Generators
     class InstallGenerator < ::Rails::Generators::Base
+      include Rails::Generators::Migration
+
       source_root File.expand_path("../templates", __FILE__)
 
       desc "Installs Traka into the app."
 
       def create_migration
-        generate "migration", "traka_change klass:string uuid:string action_type:string version:integer"
+        migration_template 'migration.rb', 'db/migrate/create_traka_changes.rb'
       end
 
       def create_version_file
--- lib/traka/change.rb
+++ lib/traka/change.rb
@@ -1,5 +1,7 @@
 module Traka
   class Change < ActiveRecord::Base
+    self.table_name = "traka_changes"
+
     attr_accessible :klass, :action_type, :uuid, :version
 
     is_traka
