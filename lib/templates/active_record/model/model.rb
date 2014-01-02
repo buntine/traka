07d87238c47b4b7200ef19a89294b4fa40caad06
Andy Buntine <bunts@hhd.com.au>
2014-01-02 16:59:52 +1100

    Removed obsolete templates

--- lib/templates/active_record/model/model.rb
+++
@@ -1,5 +0,0 @@
-class TrakaChange < ActiveRecord::Base
-  attr_accessible :klass, :action_type, :uuid, :version
-
-  is_traka
-end
--- lib/traka.rb
+++ lib/traka.rb
@@ -2,9 +2,4 @@ require 'traka/is_trakable'
 require 'traka/change'
 
 module Traka
-  class Railtie < Rails::Railtie
-    config.app_generators do |g|
-      g.templates.unshift File::expand_path('../templates', __FILE__)
-    end 
-  end
 end
